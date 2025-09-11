65be2118cd6ef21299b72a35bf45644a0e9a8a88
SamarthGlitch <samarthsajith123@gmail.com>
2025-09-11 19:06:28 +0530

    pythonswap

---
+++ Python/threenumberswap.py
@@ -0,0 +1,15 @@
+x = 5
+y = 9
+z = 1
+
+u = x
+x = z
+z = u
+
+t = y
+y = z
+z = t
+
+print("Value of x after swapping: ", x)
+print("Value of y after swapping: ", y)
+print("Value of z after swapping: ", z)
